a6fff95aa71c114391a361faaf41621c1aee9061
Krutik Arekar (MacBook Pro) <ankrutik@gmail.com>
2024-04-23 22:29:21 +0530

    src

---
+++ src/java/ankrutik/generics/GenericsBasics.java
@@ -0,0 +1,26 @@
+package ankrutik.generics;
+import java.util.ArrayList;
+
+public class GenericsBasics{
+
+    public static void demo_basics(){
+        
+        /**
+         * - Define generic list
+         * - All elements can be stored and accessed as Objects
+         * - Maintained for back support before JDK5
+         */
+        ArrayList nonSpecificList = new ArrayList();
+        nonSpecificList.add(90);
+        nonSpecificList.add("ninety");
+        nonSpecificList.add("thirty");
+        System.out.println("Printing objects saved inArrayList nonSpecificList = new ArrayList()");
+        for(Object o : nonSpecificList)
+            System.out.println(o);
+        //-------------------------
+    }
+
+    public static void main(String[] args) {
+        demo_basics();
+    }
+}
